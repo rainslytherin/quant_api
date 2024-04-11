7e6a634881399a1a44b9a9e3ea0b85e5103754a4
zhaoxinyu <xinyu7@me.com>
2024-04-11 16:54:32 +0800

    fix: 修改Dockerfile

--- build_docker.sh
+++ build_docker.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
 
-# 检查参数是否正确
-if [ $# -ne 1 ]; then
-    echo "使用方式: $0 <test|pre|prod>"
-    exit 1
-fi
-
 # 设置 Dockerfile 文件路径和生成的镜像名称
-dockerfile=""
-image_name="quant_core"
-
-# 根据参数选择 Dockerfile 和生成的镜像名称
-if [ "$1" == "pre" ]; then
-    dockerfile="Dockerfile.pre"
-    image_name="${image_name}_pre"
-elif [ "$1" == "prod" ]; then
-    dockerfile="Dockerfile.prod"
-    image_name="${image_name}_prod"
-elif [ "$1" == "test" ]; then
-    dockerfile="Dockerfile.test"
-    image_name="${image_name}_test"
-else
-    echo "参数错误: 请选择 test, pre 或 prod"
-    exit 1
-fi
+dockerfile="Dockerfile"
+image_name="quant_api"
 
 # 构建镜像
 docker build -f "$dockerfile" -t "$image_name" .
