94d8163bc51637c0c4990516425ba755fe631428
zhaoxinyu <xinyu7@me.com>
2024-04-14 15:17:58 +0800

    fix: bug fix

--- api/service.go
+++ api/service.go
@@ -71,11 +71,15 @@ func Cors() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		method := c.Request.Method
 
-		c.Header("Access-Control-Allow-Origins", "http://localhost:8080, http://121.37.182.188, http://121.37.182.188:8080")
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
+		origin := c.Request.Header.Get("Origin")
+		if origin != "" {
+			c.Header("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+			c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT")
+			c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+			c.Header("Access-Control-Allow-Credentials", "true")
+			c.Header("Access-Control-Max-Age", "172800")
+		}
 
 		//放行所有OPTIONS方法
 		if method == "OPTIONS" {
